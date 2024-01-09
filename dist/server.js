536124481501f2bca8c69832ad1c3fe341b58f3b
3DUTEX <t3xdev@gmail.com>
2024-01-09 14:20:59 -0300

    cors configurado

--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,13 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _http = require('http'); var _http2 = _interopRequireDefault(_http);
 var _socketio = require('socket.io');
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 
 const app = _express2.default.call(void 0, );
 
 app.use(_express2.default.static('./public'));
+
+app.use(_cors2.default.call(void 0, ));
 
 const serverHttp = _http2.default.createServer(app);
 
--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.static('./public'));
+
+app.use(cors());
 
 const serverHttp = http.createServer(app);
 
